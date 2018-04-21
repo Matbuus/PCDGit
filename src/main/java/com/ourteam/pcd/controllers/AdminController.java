974c4cf769d6b8a84c0f375ec7a8708d255f8e80
Malek <attiamalek2@gmail.com>
2018-04-21 17:54:25 +0100

    fail2

--- src/main/java/com/ourteam/pcd/controllers/AdminController.java
+++ src/main/java/com/ourteam/pcd/controllers/AdminController.java
@@ -201,7 +201,6 @@ public class AdminController {
 	    String originalFilename = inputFile.getOriginalFilename();
 	    Random rand = new Random();
 	    int myRandomNumber = rand.nextInt(0x10) + 0x10; // Generates a random number between 0x10 and 0x20
-	    String result = Integer.toHexString(myRandomNumber);
 	    
 	    	  // Destination ou enregistrer les fichiers sur le serveur 
 	    String suffixe =originalFilename;
